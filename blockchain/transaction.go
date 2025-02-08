931767558ecaad89fd761acd9a3bf00b27aa4f9e
Roshan310 <yuvrajaryal83@gmail.com>
2025-02-08 18:23:50 +0545

    value is of type unit64 now, NewTransaction() function added

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -8,33 +8,59 @@ import (
 	"fmt"
 	"math/big"
 	"strings"
+	"time"
 
 	"github.com/Roshan310/DaanVeer/wallet"
 )
 
 type Transactions struct {
+	TxID 		  []byte
 	SenderHash    []byte
 	RecipientHash []byte
-	Value         float32
+	Value         uint64
 	Signature     []byte
 	Timestamp     uint64
 }
 
-func NewTransaction(sender []byte, recipient []byte, value float32) *Transactions {
-	return &Transactions{SenderHash: sender, RecipientHash: recipient, Value: value}
+func NewTransaction(srcWallet *wallet.Wallet, destinationAddr string, amount uint64, chain *BlockChain) (*Transactions, error) {
+	senderAddress := string(srcWallet.Address)
+	senderPubKeyHash, err := wallet.PubKeyFromAddress(senderAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	receiverPubKeyHash, err := wallet.PubKeyFromAddress(destinationAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	newTx := Transactions{	
+		SenderHash:    senderPubKeyHash,
+		RecipientHash: receiverPubKeyHash,
+		Value: amount,
+		Timestamp: uint64(time.Now().Unix()),
+	}
+
+	newTx.TxID = newTx.Hash()
+	newTx.SignTransaction(srcWallet)
+
+	return &newTx, nil
+
 }
 
 func (t *Transactions) Print() {
 	fmt.Printf("%s\n", strings.Repeat("-", 40))
 	fmt.Printf("Sender Address:    %s\n", t.SenderHash)
 	fmt.Printf("Recipient Address: %s\n", t.RecipientHash)
-	fmt.Printf("Value:             %.1f\n", t.Value)
+	fmt.Printf("Value:             %d\n", t.Value)
 	fmt.Printf("Signature:         %x\n", t.Signature)
 	fmt.Printf("Timestamp:         %d\n", t.Timestamp)
 }
 
 func (t *Transactions) Hash() []byte {
-	m, _ := json.Marshal(t)
+	temp := *t
+	temp.Signature = nil // Exclude the signature becuase it is used only after hashing
+	m, _ := json.Marshal(temp)
 	hash := sha256.Sum256(m)
 	return hash[:]
 }
@@ -52,5 +78,6 @@ func (t *Transactions) VerifyTransaction(pubKey *ecdsa.PublicKey) bool {
 	r := new(big.Int).SetBytes(t.Signature[:len(t.Signature)/2])
 	s := new(big.Int).SetBytes(t.Signature[len(t.Signature)/2:])
 	hash := t.Hash()
-	return ecdsa.Verify(pubKey, hash, r, s)
+	fmt.Println([]byte(hash))
+	return ecdsa.Verify(pubKey, []byte(hash), r, s)
 }
