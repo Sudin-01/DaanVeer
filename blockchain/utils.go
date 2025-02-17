82ea10161a03ecc84f257e435c6b2fa8b5c8b27e
Roshan310 <yuvrajaryal83@gmail.com>
2025-02-17 16:22:04 +0545

    add utility function to get node address

--- blockchain/utils.go
+++ blockchain/utils.go
@@ -2,10 +2,45 @@ package blockchain
 
 import (
 	"log"
+	"net"
+	"errors"
 )
 
 func ShowError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getForwardSlashPosition(value string) int {
+	for i, c := range value {
+		if c == '/' {
+			return i
+		}
+	}
+
+	return -1
+}
+
+
+
+func GetNodeAddress() string {
+
+	addresses, err := net.InterfaceAddrs()
+	if err != nil {
+		ShowError(err)
+	}
+
+	for _, addr := range addresses {
+		addr_string := addr.String()
+		position := getForwardSlashPosition(addr_string)
+
+		if addr_string[:3] == "192" {
+			return addr_string[:position]
+		}
+	}
+
+	err = errors.New("Address not found")
+	log.Panic(err)
+	return ""
 }
