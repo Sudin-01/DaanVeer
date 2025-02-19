6e7bd7b5f23c648d6103f1090eddae9aee8f6f6a
Roshan310 <yuvrajaryal83@gmail.com>
2025-02-19 12:36:06 +0545

    Consensus finally works
    While hashing the block inside the ProofOfAuthority() function, the
    block was being hashed before signing the block. But VerifyProof()
    function verifies the hash after the block has a signature. So we set
    the signature field empty while hashing the block. This commit message
    could be placed on block.go but fine.

--- blockchain/consensus.go
+++ blockchain/consensus.go
@@ -3,11 +3,11 @@ package blockchain
 import (
 	"crypto/ecdsa"
 	"crypto/rand"
-	"crypto/sha256"
+	// "crypto/sha256"
 	"encoding/hex"
 	"errors"
 	"fmt"
-	// "math/big"
+	"math/big"
 
 	"github.com/Roshan310/DaanVeer/wallet"
 )
@@ -21,46 +21,42 @@ type Validator struct {
 var Validators = map[string]Validator{}
 
 
-// var Validators = map[string]bool{}
-
 func (blk *Block) VerifyProof() bool {
 	
 	//Need to debug this quite hard
 	//This will only work if VerifyHash() (inside the block.go) works
-	return true
-	// fmt.Println("About to verify the proof of block")
-	// fmt.Println("    ")
-	// validatorAddr := string(blk.ValidatorAddress)
-	// fmt.Println("Validator Address while verifying proof: ", validatorAddr)
+	//It's finally working! YEAH!!
+	// return true
+	fmt.Println("About to verify the proof of block")
+	fmt.Println("    ")
+	validatorAddr := string(blk.ValidatorAddress)
+	fmt.Println("Validator Address while verifying proof: ", validatorAddr)
 
 	// // Make sure that the validator is in the list of authorized validator
-	// validator, exists := Validators[validatorAddr]
-	// if !exists {
-	// 	fmt.Println("Block rejected: Validator is not authorized.")
-	// 	return false
-	// }
+	validator, exists := Validators[validatorAddr]
+	if !exists {
+		fmt.Println("Block rejected: Validator is not authorized.")
+		return false
+	}
 
-	// // Decode the block's signature
-	// signatureBytes, err := hex.DecodeString(blk.Signature)
-	// if err != nil {
-	// 	fmt.Println("Invalid block signature:", err)
-	// 	return false
-	// }
-	// // }
+	signatureBytes, err := hex.DecodeString(blk.Signature)
+	if err != nil {
+		fmt.Println("Invalid block signature:", err)
+		return false
+	}
 	// // Extract r and s values from the signature
-	// r := new(big.Int).SetBytes(signatureBytes[:len(signatureBytes)/2])
-	// s := new(big.Int).SetBytes(signatureBytes[len(signatureBytes)/2:])
+	r := new(big.Int).SetBytes(signatureBytes[:len(signatureBytes)/2])
+	s := new(big.Int).SetBytes(signatureBytes[len(signatureBytes)/2:])
 
-	// blockHash := blk.Hash()
-	// hashBytes := sha256.Sum256(blockHash)
+	blockHash := blk.Hash()
 	// // Verify the signature using the validator's public key
-	// if ecdsa.Verify(validator.PublicKey, hashBytes[:], r, s) {
-	// 	fmt.Println("Block verified successfully.")
-	// 	return true
-	// } else {
-	// 	fmt.Println("Block signature verification failed.")
-	// 	return false
-	// }
+	if ecdsa.Verify(validator.PublicKey, blockHash[:], r, s) {
+		fmt.Println("Block verified successfully.")
+		return true
+	} else {
+		fmt.Println("Block signature verification failed.")
+		return false
+	}
 }
 
 // ProofOfAuthority signs the block using an authorized validator's private key
@@ -73,11 +69,13 @@ func ProofOfAuthority(blk *Block, validatorWallet *wallet.Wallet) error {
 		return errors.New("validator is not authorized")
 	}
 	fmt.Println("Validator is authorized.")
+	blk.ValidatorAddress = validatorAddr
 	blockHash := blk.Hash()
-	hashBytes := sha256.Sum256(blockHash)
+	fmt.Println("Block Hash inside PoA function: ", blockHash)
+	// hashBytes := sha256.Sum256(blockHash)
 
 	// Sign the block using the validator's private key
-	r, s, err := ecdsa.Sign(rand.Reader, validatorWallet.PrivateKey, hashBytes[:])
+	r, s, err := ecdsa.Sign(rand.Reader, validatorWallet.PrivateKey, blockHash[:])
 	if err != nil {
 		return err
 	}
@@ -87,12 +85,10 @@ func ProofOfAuthority(blk *Block, validatorWallet *wallet.Wallet) error {
 	blk.Signature = hex.EncodeToString(signature)
 
 	
-	// Store the validator's address in the block
-	blk.ValidatorAddress = validatorAddr
-	fmt.Println("Validator Address: ", string(blk.ValidatorAddress))
+
+	fmt.Println("Validator Address inside the PoA function: ", string(blk.ValidatorAddress))
 	fmt.Println("Validator Address expected: ", validatorWallet.Address)
 	fmt.Println("Block signed sucess Signature: ", blk.Signature)
 
-	fmt.Println("Block signed by: ", validatorWallet.Address)
 	return nil
 }
